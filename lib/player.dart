e0bd87cdcce94f393ec8a15dac6329278e830158
Yelisei Melnichuk <yelisei.melnichuk@gmail.com>
2024-07-31 23:25:30 +0200

    add isolate - doesnt work

--- lib/player.dart
+++ lib/player.dart
@@ -1,3 +1,4 @@
+import 'dart:isolate';
 import 'dart:ui';
 
 import 'package:dart_melty_soundfont/dart_melty_soundfont.dart';
@@ -11,12 +12,12 @@ import 'package:music_notes/music_notes.dart';
 class Player {
   static const soundSampleFile = 'assets/sf2/Bosen_Korg_M1.sf2';
 
-  Synthesizer? _synth;
+  // Synthesizer? _synth;
 
   Player() {
-    _loadSoundfont().then((_) {
-      debugPrint('SoundFront loaded');
-    });
+    // _loadSoundfont().then((_) {
+    //   debugPrint('SoundFront loaded');
+    // });
 
     _loadPcmSound().then((_) {
       debugPrint('PCMSound loaded');
@@ -24,60 +25,177 @@ class Player {
   }
 
   Future<void> play() async {
+    await _initFeedingIsolate();
     await FlutterPcmSound.play();
 
-    _synth!.noteOffAll();
-    _synth!.selectPreset(channel: 0, preset: 0);
+    // _synth!.noteOffAll();
+    // _synth!.selectPreset(channel: 0, preset: 0);
   }
 
   Future<void> pause() async {
-    await FlutterPcmSound.pause();
+    await FlutterPcmSound.stop();
+    _txPort!.send('exit');
+    _isolate = null;
   }
 
   // ================== Internal Methods ======================
-  int _remainingFrames = 0;
-  int _fedCount = 0;
-  int _prevNote = 0;
+  // int _fedCount = 0;
+  // int _prevNote = 0;
 
-  void _onFeed(RootIsolateToken rootIsolateToken) async {
-    BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
-    // _remainingFrames = remainingFrames;
+  // void _onFeedClassic(int remainingFrames) async {
+  //   final notes = ScalePattern.major
+  //       .on(Note.c)
+  //       .degrees
+  //       .map((note) => note.inOctave(4).semitones)
+  //       .toList()
+  //     ..removeLast();
 
-    // c major scale
-    final notes = ScalePattern.major.on(Note.c).degrees.map((note) => note.inOctave(4).semitones).toList();
-
-    // List<int> notes = [60, 62, 64, 65, 67, 69, 71, 72];
-    int step = (_fedCount ~/ 16) % notes.length;
-    int curNote = notes[step];
-    if (curNote != _prevNote) {
-      _synth!.noteOff(channel: 0, key: _prevNote);
-      _synth!.noteOn(channel: 0, key: curNote, velocity: 100);
-    }
-    ArrayInt16 buf16 = ArrayInt16.zeros(numShorts: 1000);
-    _synth!.renderMonoInt16(buf16);
-    await FlutterPcmSound.feed(PcmArrayInt16(bytes: buf16.bytes));
-    _fedCount++;
-    _prevNote = curNote;
-  }
+  //   int step = (_fedCount ~/ 16) % notes.length;
+  //   int curNote = notes[step];
+  //   if (curNote != _prevNote) {
+  //     _synth!.noteOff(channel: 0, key: _prevNote);
+  //     _synth!.noteOn(channel: 0, key: curNote, velocity: 30);
+  //   }
+  //   ArrayInt16 buf16 = ArrayInt16.zeros(numShorts: 2000);
+  //   _synth!.renderMonoInt16(buf16);
+  //   await FlutterPcmSound.feed(PcmArrayInt16(bytes: buf16.bytes));
+  //   _fedCount++;
+  //   _prevNote = curNote;
+  // }
 
   Future<void> _loadPcmSound() async {
     await FlutterPcmSound.setup(sampleRate: 44100, channelCount: 1);
-    FlutterPcmSound.setFeedCallback((_) => compute(_onFeed, RootIsolateToken.instance!));
+    FlutterPcmSound.setFeedCallback(_requestFeed);
+    // FlutterPcmSound.setFeedCallback(_onFeedClassic);
     await FlutterPcmSound.setFeedThreshold(8000);
     await FlutterPcmSound.setLogLevel(LogLevel.standard);
   }
 
-  Future<void> _loadSoundfont() async {
-    ByteData bytes = await rootBundle.load(soundSampleFile);
-    _synth = Synthesizer.loadByteData(bytes, SynthesizerSettings());
+  // Future<void> _loadSoundfont() async {
+  //   ByteData bytes = await rootBundle.load(soundSampleFile);
+  //   _synth = Synthesizer.loadByteData(bytes, SynthesizerSettings());
 
-    // print available instruments
-    List<Preset> p = _synth!.soundFont.presets;
-    for (int i = 0; i < p.length; i++) {
-      String instrumentName = p[i].regions.isNotEmpty ? p[i].regions[0].instrument.name : "N/A";
-      debugPrint('[preset $i] name: ${p[i].name} instrument: $instrumentName');
-    }
+  //   // print available instruments
+  //   List<Preset> p = _synth!.soundFont.presets;
+  //   for (int i = 0; i < p.length; i++) {
+  //     String instrumentName =
+  //         p[i].regions.isNotEmpty ? p[i].regions[0].instrument.name : "N/A";
+  //     debugPrint('[preset $i] name: ${p[i].name} instrument: $instrumentName');
+  //   }
 
-    return Future<void>.value(null);
+  //   return Future<void>.value(null);
+  // }
+
+  Isolate? _isolate;
+  ReceivePort? _rxPort;
+  SendPort? _txPort;
+
+  Future<void> _initFeedingIsolate() async {
+    _rxPort = ReceivePort();
+    final args = FeedingIsolateArgs(
+      RootIsolateToken.instance!,
+      _rxPort!.sendPort,
+    );
+
+    _isolate = await Isolate.spawn(
+      _onFeedBackground,
+      args,
+    );
+
+    debugPrint('Waiting for isolate\'s rx port');
+
+    _rxPort!.listen(
+      (data) async {
+        if (data is SendPort) {
+          _txPort = data;
+          debugPrint('Got isolate\'s rx port');
+        } else if (data is String && data == 'requestSynthData') {
+          final bytes = await rootBundle.load(soundSampleFile);
+          _txPort!.send(bytes);
+          _requestFeed(0);
+        } else {
+          debugPrint('Message type is not handled');
+        }
+      },
+    );
   }
+
+  void _requestFeed(int remainingFrames) {
+    _txPort!.send('feed');
+  }
+}
+
+class FeedingIsolateArgs {
+  final RootIsolateToken token;
+  final SendPort sendPort;
+
+  const FeedingIsolateArgs(this.token, this.sendPort);
+}
+
+class FeedingIsolateMessage {
+  final Synthesizer? synth;
+  final int prevNote;
+  final int fedCount;
+  final int? remainingFrames;
+
+  const FeedingIsolateMessage({
+    this.synth,
+    this.remainingFrames,
+    required this.fedCount,
+    required this.prevNote,
+  });
+}
+
+void _onFeedBackground(FeedingIsolateArgs args) async {
+  BackgroundIsolateBinaryMessenger.ensureInitialized(args.token);
+
+  final txPort = args.sendPort;
+  final rxPort = ReceivePort();
+
+  final notes = ScalePattern.major
+      .on(Note.c)
+      .degrees
+      .map((note) => note.inOctave(4).semitones)
+      .toList()
+    ..removeLast();
+
+  txPort.send(rxPort.sendPort);
+
+  int fedCount = 0;
+  int prevNote = 0;
+
+  Synthesizer? synth;
+
+  txPort.send('requestSynthData');
+
+  rxPort.listen(
+    (data) async {
+      // Init synth
+      if (data is ByteData) {
+        synth = Synthesizer.loadByteData(data, SynthesizerSettings());
+
+        // print available instruments
+        List<Preset> p = synth!.soundFont.presets;
+        for (int i = 0; i < p.length; i++) {
+          String instrumentName =
+              p[i].regions.isNotEmpty ? p[i].regions[0].instrument.name : "N/A";
+          debugPrint(
+              '[preset $i] name: ${p[i].name} instrument: $instrumentName');
+        }
+      } else if (data is String && data == 'feed') {
+        int step = (fedCount ~/ 16) % notes.length;
+        int curNote = notes[step];
+        if (curNote != prevNote) {
+          synth!.noteOff(channel: 0, key: prevNote);
+          synth!.noteOn(channel: 0, key: curNote, velocity: 100);
+        }
+        ArrayInt16 buf16 = ArrayInt16.zeros(numShorts: 1000);
+        synth!.renderMonoInt16(buf16);
+        await FlutterPcmSound.feed(PcmArrayInt16(bytes: buf16.bytes));
+      } else if (data is String && data == 'exit') {
+        Isolate.exit();
+      }
+    },
+    onError: (_) => debugPrint('Feeding isolate rxPort received error'),
+  );
 }
